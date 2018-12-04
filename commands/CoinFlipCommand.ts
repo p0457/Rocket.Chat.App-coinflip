7a62d2d50ba6d52346956589c500d88d3df9969f
Thomas Gardner <thomasgardner@orthofix.com>
2018-12-04 10:20:29 -0600

    Fix command and settings
    That's what I get for copy-pasting

--- commands/CoinFlipCommand.ts
+++ commands/CoinFlipCommand.ts
@@ -5,7 +5,7 @@ import { CoinFlipGetter } from '../helper/CoinFlipGetter';
 import { CoinFlipResult } from '../helper/CoinFlipResult';
 
 export class CoinFlipCommand implements ISlashCommand {
-    public command: string = 'abstract';
+    public command: string = 'coinflip';
     public i18nParamsExample: string = 'Slash_Command_Params_Example';
     public i18nDescription: string = 'Slash_Command_Description';
     public providesPreview: boolean = false;
@@ -13,12 +13,12 @@ export class CoinFlipCommand implements ISlashCommand {
     constructor(private readonly coinFlipGetter: CoinFlipGetter) { }
 
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-        const icon = await read.getEnvironmentReader().getSettings().getValueById('abstract_icon');
-        const username = await read.getEnvironmentReader().getSettings().getValueById('abstract_name');
+        const icon = await read.getEnvironmentReader().getSettings().getValueById('coinflip_icon');
+        const username = await read.getEnvironmentReader().getSettings().getValueById('coinflip_name');
 
         let result = new CoinFlipResult();
         result = await this.coinFlipGetter.flip();
-        const text = "Coin was flipped! Looks like it was " + result.result;
+        const text = 'Coin was flipped! Looks like it was ' + result.result;
 
         const builder = modify.getCreator().startMessage()
             .setSender(context.getSender()).setRoom(context.getRoom())
